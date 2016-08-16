bba2a82c45ccf75e6e86f25e53415178a9ec3118
729889375@qq.com <729889375@qq.com>
2016-08-16 16:26:52 +0800

    修改

--- tieba.py
+++ tieba.py
@@ -51,22 +51,22 @@ def sguTieBa(content):
 '''提取要发送的内容'''
 def theData(time):
     toSendData={
-                '16':u'准时冒一下泡！by the way 我叫小号 ^_^',
-                '17':u'收到来自小号的调戏，你接受吗？',
-                '18':u'嘿嘿，小鲜肉看过来。这里很精彩哦！（不要回此贴，老板会骂我的:)）',
-                '19':u'又是一天了，小号在极光等着各位的到来哦！',
-                '20':u'曾经,有一份真挚的\'爱情\'摆在我面前,我没有好好珍惜,等到我失去的时候才后悔莫及...',
-                '21':u'没错,我是小号，想成为大神吗？赶紧加极光工作室交流群吧！群号:539365225',
-                '22':u'想成为大神吗？想让代码在你的指尖飞跃吗？别犹豫了，赶紧拿起手机加群吧！(ps:群号里面有)',
-                '23':u'小号例行插楼，我只是个机器人，别摸我头！',
-                '24':u'还有人没睡吗？过来吧，和小号面基吧！',
-                '25':u'啦啦啦，还没睡的小伙伴，下一个大神就是你了！',
-                '26':u'临近开学了，不知道各位新鲜肉有什么想法呢？路过的快进来看看啊，小号要开车了！',
-                '27':u'手打很累的，没人么？没人我可睡了:(',
-                '28':u'嘿，没错是我小号，你们都睡了吗？没人陪我玩啊:(',
-                '29':u'马上要开学了，很快就能见到新鲜滚烫烫的小鲜肉了，好嗨心！！',
-                '30':u'程序员你还怕没对象吗？new一个吧!',
-                '31':u'九月快到了，你们还会远吗？快到碗里来！！',
+                16:u'准时冒一下泡！by the way 我叫小号 ^_^',
+                17:u'收到来自小号的调戏，你接受吗？',
+                18:u'嘿嘿，小鲜肉看过来。这里很精彩哦！（不要回此贴，老板会骂我的:)）',
+                19:u'又是一天了，小号在极光等着各位的到来哦！',
+                20:u'曾经,有一份真挚的\'爱情\'摆在我面前,我没有好好珍惜,等到我失去的时候才后悔莫及...',
+                21:u'没错,我是小号，想成为大神吗？赶紧加极光工作室交流群吧！群号:539365225',
+                22:u'想成为大神吗？想让代码在你的指尖飞跃吗？别犹豫了，赶紧拿起手机加群吧！(ps:群号里面有)',
+                23:u'小号例行插楼，我只是个机器人，别摸我头！',
+                24:u'还有人没睡吗？过来吧，和小号面基吧！',
+                25:u'啦啦啦，还没睡的小伙伴，下一个大神就是你了！',
+                26:u'临近开学了，不知道各位新鲜肉有什么想法呢？路过的快进来看看啊，小号要开车了！',
+                27:u'手打很累的，没人么？没人我可睡了:(',
+                28:u'嘿，没错是我小号，你们都睡了吗？没人陪我玩啊:(',
+                29:u'马上要开学了，很快就能见到新鲜滚烫烫的小鲜肉了，好嗨心！！',
+                30:u'程序员你还怕没对象吗？new一个吧!',
+                31:u'九月快到了，你们还会远吗？快到碗里来！！',
                }
     return toSendData[time]
 
@@ -90,13 +90,13 @@ def send_mail(tolist,sub,content):
 
 
 '''发送回执邮件给我'''
-def sendEmailToMe(resultcode,data,message=u'报告老板，帖子发送成功!:)'):
+def sendEmailToMe(resultcode,data,message=u'报告老板，帖子发送成功:)'):
 
     if resultcode==0:
-         content=message+u'发帖的内容为:'+data
+         sub=message+u'发帖的内容为:'+data
     else:
-         content=u'噢！I\'m sorry Boss,发生了未知的错误，发帖失败!:('
-    send_mail(mailto_list,u'来自小号的消息',content)
+         sub=u'噢！I\'m sorry Boss,发生了未知的错误，发帖失败!:('
+    send_mail(mailto_list,sub,'')
 
 if __name__=='__main__':
      host = 'pop.sina.com'
@@ -110,15 +110,22 @@ if __name__=='__main__':
      while(True):
           nowDay=time.strftime("%d",time.localtime())
           nowHour=time.strftime("%H",time.localtime())
-          nowHour=int(nowHour)
+          nowMinute=time.strftime("%M",time.localtime())
+          nowSeconds=time.strftime("%S",time.localtime())
+
           nowDay=int(nowDay)
-          if nowDay%24==nowHour:
-              # 提取要发送的内容
-              data=theData()
+          nowHour=int(nowHour)
+          nowMinute=int(nowMinute)
+          nowSeconds=int(nowSeconds)
+
+          if nowDay%24==nowHour and nowMinute==15 and nowSeconds==15:
+              #提取要发送的内容
+              data=theData(nowDay)
               # 发送帖子
               resultcode=sguTieBa(data)
               # 判断发送帖子的返回码，发送邮件通知
               sendEmailToMe(resultcode,data)
+              time.sleep(2)
 
           if nowDay==1:
               exit(1)
